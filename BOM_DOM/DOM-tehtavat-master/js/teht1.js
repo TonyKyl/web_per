1a4df9cdb65ddcb9bdd8cd940d0b67cde1a761d5
TonyKyl <44458863+TonyKyl@users.noreply.github.com>
2018-11-06 10:44:41 +0200

    funktioita

---
+++ BOM_DOM/DOM-tehtavat-master/js/teht1.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementsByTagName("td")[0].innerHTML = "Tony";
+  document.getElementsByTagName("td")[1].innerHTML = "Kylander";
+  document.getElementsByTagName("td")[2].innerHTML = "Touner";
+  document.getElementsByTagName("td")[3].innerHTML = "Epsoonpolku 7 Ö 7, 00000 Epsoo";
+  document.createElement('th'[5].appendChild;
+  document.getElementsByTagName("th")[5].innerHTML = "Puhelinnumero";
+
+}
